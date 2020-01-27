3dc67ea5894a036334439add4601431417e4f6e6
Admin <alan.escalera@fundacion-jala.org>
2020-01-27 09:58:34 -0400

    update decode numbers

--- src/main/java/DecodeNumber.java
+++ src/main/java/DecodeNumber.java
@@ -1,40 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class DecodeNumber {
-    private final String oneNumber = "     |  |";
-    private final String twoNumber = " _  _||_ ";
-    private final String threeNumber = " _  _| _|";
-    private final String fourNumber = "   |_|  |";
-    private final String fiveNumber = " _ |_  _|";
-    private final String sixNumber = " _ |_ |_|";
-    private final String sevenNumber = " _   |  |";
-    private final String eightNumber = " _ |_||_|";
-    private final String nineNumber = " _ |_| _|";
-    private final String zeroNumber = " _ | ||_|";
+    private final String ONE = "     |  |";
+    private final String TWO = " _  _||_ ";
+    private final String THREE = " _  _| _|";
+    private final String FOUR = "   |_|  |";
+    private final String FIVE = " _ |_  _|";
+    private final String SIX = " _ |_ |_|";
+    private final String SEVEN = " _   |  |";
+    private final String EIGHT = " _ |_||_|";
+    private final String NINE = " _ |_| _|";
+    private final String ZERO = " _ | ||_|";
+    private final String QUESTION_MARK = "?";
 
     public String convertToNumber(String numbers) {
 
-        switch (numbers){
-            case oneNumber:
-                return "1";
-            case threeNumber:
-                return "3";
-            case twoNumber:
-                return "2";
-            case fourNumber:
-                return "4";
-            case fiveNumber:
-                return "5";
-            case sixNumber:
-                return "6";
-            case sevenNumber:
-                return "7";
-            case eightNumber:
-                return "8";
-            case nineNumber:
-                return "9";
-            case zeroNumber:
-                return "0";
-            default:
-                return "?";
-        }
+        Map map = new HashMap<>();
+        map.put(ONE, "1");
+        map.put(TWO, "2");
+        map.put(THREE, "3");
+        map.put(FOUR, "4");
+        map.put(FIVE, "5");
+        map.put(SIX, "6");
+        map.put(SEVEN, "7");
+        map.put(EIGHT, "8");
+        map.put(NINE, "9");
+        map.put(ZERO, "0");
+
+        return  map.getOrDefault(numbers,QUESTION_MARK).toString();
     }
 }
